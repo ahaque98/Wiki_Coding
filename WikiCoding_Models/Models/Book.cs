fd520e1874298f477d85cf0e201491ba94316423
vikingslord <vikingslord98@gmail.com>
2023-07-11 22:18:06 +0600

    MaxLength and NotMapped

--- WikiCoding_Models/Models/Book.cs
+++ WikiCoding_Models/Models/Book.cs
@@ -1,18 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WikiCoding_Model.Models
 {
     public class Book
     {
-        [Key]
+        [Key] //primary-key
         public int Id { get; set; }
-        public string Title { get; set; } 
+        public string Title { get; set; }
+        [MaxLength(20)] //the ISBN cannot be more than 20 characters
         public string ISBN { get; set; }
         public decimal Price { get; set; }
+
+        [NotMapped] //table won't be created but will function as a db_table
+        public string PriceRange { get; set; }
+
     }
 }
