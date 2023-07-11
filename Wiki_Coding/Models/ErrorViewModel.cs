641c206ec0feb1b0bad8e70f033edeadc8680362
vikingslord <vikingslord98@gmail.com>
2023-07-11 13:04:50 +0600

    Add project files.

---
+++ Wiki_Coding/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Wiki_Coding.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
