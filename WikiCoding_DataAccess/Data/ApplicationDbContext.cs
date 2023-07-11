b3b45cefb55bf6221622f3a09d072f066ea0885e
vikingslord <vikingslord98@gmail.com>
2023-07-11 17:37:15 +0600

    Seed Data

---
+++ WikiCoding_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WikiCoding_Model.Models;
+
+namespace WikiCoding_DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().Property(data => data.Price).HasPrecision(10, 5);
+
+            //seeding Data
+            modelBuilder.Entity<Book>().HasData(
+                new Book
+                {
+                    Id = 1,
+                    Title = "The new tale",
+                    ISBN = "1100001584279",
+                    Price = 12.98m
+                },
+
+                new Book
+                {
+                    Id = 2,
+                    Title = "once upon a time",
+                    ISBN = "01561216803",
+                    Price = 34.23m
+                }
+            );
+
+            var bookList = new Book[]
+            {
+                new Book
+                {
+                    Id=3,
+                    Title="Making a new faith",
+                    ISBN="01725797413",
+                    Price=32.45m
+                },
+
+                new Book
+                {
+                    Id=4,
+                    Title="Big Star",
+                    ISBN="10892163360",
+                    Price=22.41m
+                },
+                new Book
+                {
+                    Id=5,
+                    Title="B&H",
+                    ISBN="9877654",
+                    Price=11.21m
+                }
+            };
+
+            modelBuilder.Entity<Book>().HasData(bookList); //passing bookList array to Db (Seeding)
+        }
+    }
+}
---
+++ WikiCoding_DataAccess/Migrations/20230711113639_seedBookTable.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WikiCoding_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedBookTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "ISBN", "Price", "Title" },
+                values: new object[,]
+                {
+                    { 1, "1100001584279", 12.98m, "The new tale" },
+                    { 2, "01561216803", 34.23m, "once upon a time" },
+                    { 3, "01725797413", 32.45m, "Making a new faith" },
+                    { 4, "10892163360", 22.41m, "Big Star" },
+                    { 5, "9877654", 11.21m, "B&H" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
