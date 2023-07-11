60b0916d36f523cfccfbd3b7838e8f94da866617
vikingslord <vikingslord98@gmail.com>
2023-07-11 18:15:21 +0600

    Update Table and Column Name

---
+++ WikiCoding_DataAccess/Migrations/20230711115627_updateGenreTableDisplayOrderColumn.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WikiCoding_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateGenreTableDisplayOrderColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Genres",
+                table: "Genres");
+
+            migrationBuilder.RenameTable(
+                name: "Genres",
+                newName: "tb_genres");
+
+            migrationBuilder.RenameColumn(
+                name: "DisplayOrder",
+                table: "tb_genres",
+                newName: "Display Order");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_tb_genres",
+                table: "tb_genres",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_tb_genres",
+                table: "tb_genres");
+
+            migrationBuilder.RenameTable(
+                name: "tb_genres",
+                newName: "Genres");
+
+            migrationBuilder.RenameColumn(
+                name: "Display Order",
+                table: "Genres",
+                newName: "DisplayOrder");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Genres",
+                table: "Genres",
+                column: "Id");
+        }
+    }
+}
